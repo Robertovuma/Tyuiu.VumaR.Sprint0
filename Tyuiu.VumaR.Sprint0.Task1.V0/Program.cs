f98fe5e967b21c9e9c9230f1511965aebc1857dd
Роберту вума <Tyuiu.vumaroberto@outlook.com>
2024-11-28 19:00:29 +0300

    Task1

---
+++ Tyuiu.VumaR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.VumaR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+
+            string name = "Two9";
+            int age = 50;
+
+            Console.WriteLine("Здравствуйте  меня зовут " + name +  " мне " + age + " лет ");
+            Console.ReadKey();
+
+        }
+    }
+}
