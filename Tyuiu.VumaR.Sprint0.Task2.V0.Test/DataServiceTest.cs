7581bbd1129986c805d3de6cd6e3042194c73434
Роберту вума <Tyuiu.vumaroberto@outlook.com>
2024-11-28 19:55:37 +0300

    Task2

---
+++ Tyuiu.VumaR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.VumaR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VumaR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Two9";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("привет, Two9", res);
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Tyuiu.VumaR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VumaR.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Two9"));
+            Console.ReadKey();
+        }
+    }
+}
